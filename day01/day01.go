3d8e791aacc64d5e87dd45d8cd3d98c014c89c2d
Alwaysgone <ffxiizocker@hotmail.com>
2025-01-11 16:34:48 +0100

    Added solution for day01 part01

---
+++ day01/day01.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func diff(a, b int) int {
+	if a < b {
+		return b - a
+	}
+	return a - b
+}
+
+func main() {
+	f, err := os.OpenFile("input/input.txt", os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalf("open file error: %v", err)
+		return
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	firstSet := []int{}
+	secondSet := []int{}
+	for sc.Scan() {
+		parts := strings.Fields(sc.Text())
+		val, err := strconv.Atoi(parts[0])
+		if err != nil {
+			panic(err)
+		}
+		firstSet = append(firstSet, val)
+
+		val, err = strconv.Atoi(parts[1])
+		if err != nil {
+			panic(err)
+		}
+		secondSet = append(secondSet, val)
+	}
+	part01(firstSet, secondSet)
+}
+
+func part01(firstSet []int, secondSet []int) {
+	slices.Sort(firstSet)
+	slices.Sort(secondSet)
+	diffSum := 0
+	for i := range firstSet {
+		diffSum += diff(firstSet[i], secondSet[i])
+	}
+	fmt.Println(diffSum)
+}
---
+++ day01/go.mod
@@ -0,0 +1,3 @@
+module adventofcode2024/day01
+
+go 1.21.6
