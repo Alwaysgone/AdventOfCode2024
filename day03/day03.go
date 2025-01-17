e3fe20d046ca6a8905dbeec778607c959f9a7f8c
Alwaysgone <ffxiizocker@hotmail.com>
2025-01-18 00:21:56 +0100

    added solution for day03 part 2

--- day03/day03.go
+++ day03/day03.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -30,14 +31,14 @@ func main() {
 	sc := bufio.NewScanner(f)
 
 	part01(sc)
-	/*f2, err2 := os.OpenFile("input/input.txt", os.O_RDONLY, os.ModePerm)
+	f2, err2 := os.OpenFile("input/input.txt", os.O_RDONLY, os.ModePerm)
 	if err2 != nil {
 		log.Fatalf("open file error: %v", err2)
 		return
 	}
 	defer f2.Close()
 	sc2 := bufio.NewScanner(f2)
-	part02(sc2)*/
+	part02(sc2)
 }
 
 func part01(sc *bufio.Scanner) {
@@ -57,39 +58,66 @@ func part01(sc *bufio.Scanner) {
 	fmt.Println(mulSum)
 }
 
+type instruction struct {
+	index  int
+	enable bool
+}
+
+func indexAt(s, substring string, n int) int {
+	idx := strings.Index(s[n:], substring)
+	if idx > -1 {
+		idx += n
+	}
+	return idx
+}
+
 func part02(sc *bufio.Scanner) {
-
-}
-
-func isSafeReport(report []int) (bool, int) {
-	lastDiffValue := report[0] - report[1]
-	if !safeDiff(lastDiffValue) {
-		return false, 0
+	mulSum := 0
+	line := ""
+	for sc.Scan() {
+		line += sc.Text()
 	}
-	for i := 2; i < len(report); i++ {
-		currentDiffValue := report[i-1] - report[i]
-		sameSign := (lastDiffValue * currentDiffValue) > 0
-		if !sameSign || !safeDiff(currentDiffValue) {
-			return false, i - 1
+	instructions := []instruction{}
+
+	currentIndex := 0
+	for {
+		currentIndex = indexAt(line, `do()`, currentIndex)
+		if currentIndex < 0 {
+			break
 		}
-		lastDiffValue = currentDiffValue
+		instructions = append(instructions, instruction{currentIndex, true})
+		currentIndex++
 	}
-	return true, -1
-}
-
-func isSafeReport2(report []int) bool {
-	lastDiffValue := report[0] - report[1]
-	badLevelCount := 0
-	if !safeDiff(lastDiffValue) {
-		badLevelCount++
-	}
-	for i := 2; i < len(report); i++ {
-		currentDiffValue := report[i-1] - report[i]
-		sameSign := (lastDiffValue * currentDiffValue) > 0
-		if !sameSign || !safeDiff(currentDiffValue) {
-			badLevelCount++
+	currentIndex = 0
+	for {
+		currentIndex = indexAt(line, `don't()`, currentIndex)
+		if currentIndex < 0 {
+			break
 		}
-		lastDiffValue = currentDiffValue
+		instructions = append(instructions, instruction{currentIndex, false})
+		currentIndex++
 	}
-	return badLevelCount <= 1
+	sort.Slice(instructions[:], func(i, j int) bool {
+		return instructions[i].index < instructions[j].index
+	})
+
+	instructionIndex := 0
+	enabled := true
+	reg, _ := regexp.Compile(`mul\([1-9][0-9]{0,2}\,[1-9][0-9]{0,2}\)`)
+	indexes := reg.FindAllStringIndex(line, -1)
+	for index, match := range reg.FindAllString(line, -1) {
+		for instructionIndex < len(instructions) && instructions[instructionIndex].index <= indexes[index][0] {
+			enabled = instructions[instructionIndex].enable
+			instructionIndex++
+		}
+		if enabled {
+			commaIndex := strings.Index(match, ",")
+			val1 := match[4:commaIndex]
+			val2 := match[commaIndex+1 : len(match)-1]
+			v1, _ := strconv.Atoi(val1)
+			v2, _ := strconv.Atoi(val2)
+			mulSum += v1 * v2
+		}
+	}
+	fmt.Println(mulSum)
 }
